9d5804ec0efa9a548774ee6dd2bd62026a214952
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-05-19 13:55:19 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from pyrogram import Client
+
+BOT_TOKEN = # your bot token from telegram.me/BotFather. Sample :- "12345:abcdefghijklmnop"
+API_ID = # your api id from my.telegram.org. Sample :- int("123456")
+API_HASH = # your api hash from my.telegram.org Sample :- "fayasnoushad123"
+
+FayasNoushad = Client(
+    "Simple-Pyrogram-Bot",
+    bot_token=BOT_TOKEN,
+    api_id=int(API_ID),
+    api_hash=API_HASH
+)
+
+FayasNoushad.run()
