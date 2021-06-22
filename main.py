fab710c9b69322892924925547d191e25d1efdfe
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-22 17:20:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ BOT_TOKEN = # your bot token from telegram.me/BotFather. Sample :- "12345:abcdef
 API_ID = # your api id from my.telegram.org. Sample :- int("123456")
 API_HASH = # your api hash from my.telegram.org Sample :- "fayasnoushad123"
 
-FayasNoushad = Client(
+Bot = Client(
     "Simple-Pyrogram-Bot",
     bot_token=BOT_TOKEN,
     api_id=int(API_ID),
     api_hash=API_HASH
 )
 
-@FayasNoushad.on_message(filters.command(["start"]))
+@Bot.on_message(filters.command(["start"]))
 async def start(bot, update):
     await update.reply_text(
         text=f"Hi {update.from_user.mention}"
     )
 
-FayasNoushad.run()
+Bot.run()
